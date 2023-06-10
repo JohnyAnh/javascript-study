5659085cdfbb949462f37cd7ff00a5fae84bae14
JohnyAnh <johnyanhn01@gmail.com>
2023-06-10 12:26:30 +0700

    Datatype

---
+++ Chapter2ss3/ss3.js
@@ -0,0 +1,15 @@
+//Khai bao bien dung let no tu hieu
+let c = 5; // now c is number
+let d = 5.9;//  now c is number
+let e = 'Hello';//  now e is string
+let f = `Hello 1`;//  now e is string
+// Dung 3 loai dau thoai mai ''; ""; ``
+
+let x = 5;
+let y = 5;
+let z = 6;
+
+console.log('Hello from Javascript:', x == y, y == z)
+
+console.log("Test String: " ,'x=',x, 'y=',y,'z=',z)//muoons in gi them dau ,
+
