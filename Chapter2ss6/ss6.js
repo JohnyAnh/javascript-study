24ef1df10bcc4bcc4cf41792e8fae40c2adefdbe
JohnyAnh <johnyanhn01@gmail.com>
2023-06-12 03:53:54 +0700

    Numbers

---
+++ Chapter2ss6/ss6.js
@@ -0,0 +1,19 @@
+console.log('Hello world From JavaScript'.toUpperCase())//Ham co san chuyen thanh chu in hoa
+let a = 5;
+let b = 6;
+let c = 6.9;
+let d = '5';
+//Meo
+let e = +d;// tuong duong let e = Number('d'); chuyen doi chuoi thanh number
+console.log('e value: ', e, 'type: ', typeof e);//e value:  5 type:  number
+
+
+
+console.log(a + b);//11
+console.log(b + c);//12.9
+console.log(c + d);//6.9
+console.log(a * d);//25
+console.log(a - d);//0
+console.log(a === d, typeof a, 'd:', typeof d);//false 'number' 'd:' 'string'
+
+
