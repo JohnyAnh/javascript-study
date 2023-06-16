11769f1bbcd38a4c76a8d6b153cea817c241566b
JohnyAnh <johnyanhn01@gmail.com>
2023-06-16 09:57:38 +0700

    For loops

---
+++ ct3ss1/ss1.js
@@ -0,0 +1,16 @@
+console.log("Hello Control Flow Basics");
+
+let arr = ['Mu', 'MCT', 'Liber', 'Chelsea'];
+for (let i = 0; i < arr.length; i++) {
+    console.log('Check value: ' ,arr[i]);
+    
+}
+
+let i = 0;
+for (let i = 0; i < 10; i++) {
+    console.log('Check value: ', i);
+    
+}
+
+console.log('Check value i: ' ,i);
+// Quy tắc code khai báo biết bằng let 
