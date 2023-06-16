3a58e4dcc829a15474ffdfc3f5f15e93b1d6ac9a
JohnyAnh <johnyanhn01@gmail.com>
2023-06-16 09:25:11 +0700

    So sánh ==, ===,  Empty, null, undefined

---
+++ ct2ss10/ss10.js
@@ -0,0 +1,20 @@
+console.log("Hello SS10");
+
+let a = 5; // x <- y
+let b = '5';
+
+// === type, value convention
+// == value
+console.log('compare a,b ','a = ', a, 'b = ', b, 'result', a == b);
+console.log('compare a,b ','a = ', a, 'b = ', b, 'result', a === b);
+
+
+// Empty, null, undefinedlet ;
+// Empty: Rỗng khi khai báo biến = rỗng không để giá trị 
+// undefinedlet: khi khai biến không có giá trị
+let c = null;
+console.log('check value: ',c , typeof c);
+let d = '';
+d = 5;
+d = ['11']
+console.log('check value: ',d , typeof d);
