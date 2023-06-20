c2f61ef707cd182c2bbcb6c9e907963a0c0a0d09
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-06-20 14:03:32 +0700

    Arrow Functions - Đi Săn Với JS

---
+++ ct4ss3/ss3.js
@@ -0,0 +1,11 @@
+console.log('Hello JavaScript Arrow Functions - Đi Săn Với JS');
+
+//  function sum(a, b){
+//     return a+b;
+//  }
+
+//arrow function
+let sum = (a, b) => {
+    return a + b;
+}
+ console.log('Check Sum: ', sum(6, 9));
